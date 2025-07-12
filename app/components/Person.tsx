f8e351c63f74e6de7b4686878b0b4a5a7ab56dc8
Rebaone Khanyisile Cynthia Vilakazi <144230015+KhanyiCodes@users.noreply.github.com>
2025-07-12 11:27:49 +0200

    Add MyButton component and styles; remove empty Person component and type

---
+++ app/components/Person.tsx
@@ -0,0 +1,15 @@
+import type Person from "~/types/Person";
+
+export default function Profile({ person }: { person: Person }) {
+  return (
+    <>
+      <h1>{person.name}</h1>
+      <img
+        className="avatar"
+        src={person.imageUrl}
+        alt={person.name}
+        style={{ width: person.imageSize, height: person.imageSize }}
+      />
+    </>
+  );
+}
